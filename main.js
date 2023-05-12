1807460b9db0aa03c32de17f4587d270e3aa33f8
Igor Babko <i.i.babko@gmail.com>
2023-05-12 20:46:48 +0300

    import web worker ?worker (for now doesn't work in firefox)

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 import imgUrl from '@/img/javascript.svg'
 import scriptUrl from './counter.js?url'
 import scriptAsString from './counter.js?raw'
+import MyWorker from './worker.js?worker'
 
 console.log(imgUrl)
 console.log(scriptUrl)
 console.log(scriptAsString)
+console.log(MyWorker)
+
+new MyWorker()
 
 document.getElementById('img').src = imgUrl
---
+++ worker.js
@@ -0,0 +1,3 @@
+let counter = 0
+
+setInterval(() => console.log(counter++), 1000)
