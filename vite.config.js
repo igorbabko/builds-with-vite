b26e53f36fc0b06a7d4dedfa97e3f6e67feb087b
Igor Babko <i.i.babko@gmail.com>
2023-05-12 20:03:52 +0300

    aliases in css files

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 
+import path from 'path'
+
 export default defineConfig({
   // build: {
   //   cssCodeSplit: false,
   // },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/assets'),
+    },
+  },
   esbuild: {
     jsxFactory: 'create',
     jsxFragment: 'frag',
