205ce59616fa2fd0c13b1f49ae2edbf57da0872f
Igor Babko <i.i.babko@gmail.com>
2023-05-12 12:49:43 +0300

    preprocessor sass scss
    preprocessor sass

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-import styles from './src/assets/style.css?inline'
-import styles2 from './src/assets/style2.css'
-
-console.log(styles)
-console.log(styles2)
-
-const styleEl = document.createElement('style')
-
-styleEl.innerText = styles
-
-document.querySelector('head').append(styleEl)
+import './src/assets/style.scss'
