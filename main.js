dafe4882d1b258b67df95fc6481975e1d2c954e0
Igor Babko <i.i.babko@gmail.com>
2023-05-13 10:34:57 +0300

    glob imports

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 import '@/style.scss'
 
-const modules = import.meta.glob('./src/*.js')
+// const modules = import.meta.glob('./src/*.js')
+const modules1 = import.meta.glob('./src/a.js', { import: 'a1', eager: true })
+const modules2 = import.meta.glob('./src/*.js', { as: 'raw', eager: true })
+const modules3 = import.meta.glob('./src/*.js')
 
-console.log(modules)
+console.log(modules1)
 
-for (const path in modules) {
-  modules[path]().then((mod) => {
+for (const path in modules3) {
+  modules3[path]().then((mod) => {
     console.log(path, mod)
   })
 }
--- src/a.js
+++ src/a.js
@@ -1,1 +1,4 @@
 console.log('module a')
+
+export const a1 = 1;
+export const a2 = 2;
