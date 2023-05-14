a7568246bbed1a454f128cf2fd3cd581105ee702
Igor Babko <i.i.babko@gmail.com>
2023-05-14 10:09:12 +0300

    04. CSS modules

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 import './src/assets/03.css'
+import { heading as headingFontSize } from './src/assets/04a.module.css'
+import { heading as headingColor } from './src/assets/04b.module.css'
+
+document.querySelector('.heading').className = `${headingFontSize} ${headingColor}`
