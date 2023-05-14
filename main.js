63a930e6473c8238da411ac9b9dd2a44ce0d87e8
Igor Babko <i.i.babko@gmail.com>
2023-05-14 10:47:56 +0300

    05. PostCSS plugins (TailwindCSS installation)

--- main.js
+++ main.js
@@ -1,5 +1,1 @@
-import './src/assets/03.css'
-import { heading as headingFontSize } from './src/assets/04a.module.css'
-import { heading as headingColor } from './src/assets/04b.module.css'
-
-document.querySelector('.heading').className = `${headingFontSize} ${headingColor}`
+import './src/assets/05.css'
