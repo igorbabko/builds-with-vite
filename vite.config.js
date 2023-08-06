ef5d32df611df04d7b575f294fd00691f0395b1c
Igor Babko <i.i.babko@gmail.com>
2023-08-06 11:17:38 +0300

    x: server options

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,25 @@ import Csv from './vite-plugin-csv'
 import path from 'path'
 
 export default {
+  server: {
+    port: '3333',
+    strictPort: true,
+    open: true, // '/api/products',
+    headers: {
+      'a': 'b'
+    },
+    proxy: {
+      // string shorthand: http://localhost:5173/foo -> http://localhost:4567/foo
+      // '/products': 'https://dummyjson.com',
+
+      // with options: http://localhost:5173/api/bar-> http://jsonplaceholder.typicode.com/bar
+      '/api': {
+        target: 'https://dummyjson.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
   esbuild: {
     jsxFactory: 'create',
     jsxInject: 'import { create } from "/src/12-create.js"'
