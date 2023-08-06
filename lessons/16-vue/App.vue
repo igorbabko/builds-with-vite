ef92d52c09b4435bf8a45bc43f9846e133a40640
Igor Babko <i.i.babko@gmail.com>
2023-08-06 20:49:42 +0300

    x: vue

---
+++ lessons/16-vue/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+const counter = ref(0)
+</script>
+
+<template>
+  <h1>App component</h1>
+  <button @click="counter++">Count: {{ counter }}</button>
+</template>
+
+<style scoped>
+h1 {
+  color: indigo;
+}
+</style>
