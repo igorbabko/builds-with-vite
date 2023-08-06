3ae6ddf12ff42d29361cd7b339be6797df749018
Igor Babko <i.i.babko@gmail.com>
2023-08-06 17:20:55 +0300

    x: client communication

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 import products from './src/assets/products.csv'
-import './src/assets/12.css'
 
 document.querySelector('pre').textContent = JSON.stringify(products)
 
 if (import.meta.hot) {
   import.meta.hot.on('csv-update', ({ url, data }) => {
     // console.log(data)
-    console.log(`[vite] hot updated: ${url}`);
+    console.log(`[vite] hot updated: ${url}`)
 
     document.querySelector('pre').textContent = JSON.stringify(data)
+  })
+
+  import.meta.hot.on('connected', (message) => {
+    console.log(message)
+
+    import.meta.hot.send('ping', 'Hello server!')
+  })
+
+  import.meta.hot.on('pong', (message) => {
+    console.log(message)
   })
 }
