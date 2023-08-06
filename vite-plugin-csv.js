611a166f4df2d3c34682c6211f01d2bf26da3b8f
Igor Babko <i.i.babko@gmail.com>
2023-08-06 11:17:15 +0300

    x: plugin options

--- vite-plugin-csv.js
+++ vite-plugin-csv.js
@@ -1,17 +1,36 @@
 import { parse } from 'csv-parse/sync'
 
-export default () => ({
-  name: 'vite:csv',
-  async transform(src, id) {
-    if (/\.csv$/.test(id)) {
-      // console.log(src, id)
-      // console.log(records)
+export default () => {
+  let config = null
 
-      const records = parse(src, { columns: true })
+  return {
+    name: 'csv',
+    // apply: 'serve', // 'build'
+    apply(config, { command, mode }) {
+      // console.log(config)
 
-      return {
-        code: `export default ${JSON.stringify(records)}`
+      return mode === 'development'
+    },
+    configResolved(resolvedConfig) {
+      config = resolvedConfig
+    },
+    async transform(src, id) {
+      const columns = config.command === 'serve'
+
+      if (/\.csv$/.test(id)) {
+        // console.log(src, id)
+        // console.log(records)
+
+        const records = parse(src, { columns })
+
+        return {
+          code: `export default ${JSON.stringify(records)}`
+        }
       }
     }
+    // transformIndexHtml(html) {
+    //   console.log('--------------------------------------');
+    //   return html.replace(/<\/body>/, `<script>alert('aa')</script><\/body>`)
+    // },
   }
-})
+}
