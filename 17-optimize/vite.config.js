402f9bbba6add81b830bd54ba9a957267fe4f191
Igor Babko <i.i.babko@gmail.com>
2023-08-30 11:23:09 +0300

    17. multi page

--- 17-optimize/vite.config.js
+++ 17-optimize/vite.config.js
@@ -1,5 +1,14 @@
 import Inspect from 'vite-plugin-inspect'
+import { resolve } from 'path'
 
 export default {
-  plugins: [Inspect()]
+  plugins: [Inspect()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login/index.html')
+      }
+    }
+  }
 }
