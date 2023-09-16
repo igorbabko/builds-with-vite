2dba702c38a14dbc6056d49ed569cd202b07007e
Igor Babko <i.i.babko@gmail.com>
2023-09-16 08:38:19 +0300

    .

--- 24-creating-plugin/vite-plugin-csv.js
+++ 24-creating-plugin/vite-plugin-csv.js
@@ -1,14 +1,33 @@
 import { parse } from 'csv-parse/sync'
 
-export default () => ({
-  name: 'csv',
-  transform(src, id) {
-    if (/\.csv$/.test(id)) {
-      const records = parse(src, { columns: true })
+export default () => {
+  let config = null
 
-      return {
-        code: `export default ${JSON.stringify(records)}`
+  return {
+    name: 'csv',
+    // apply: 'serve', // 'build'
+    apply(config, { command, mode }) {
+      // console.log(config)
+
+      return mode === 'development'
+    },
+    configResolved(resolvedConfig) {
+      config = resolvedConfig
+    },
+    transform(src, id) {
+      const columns = config.command === 'serve'
+
+      if (/\.csv$/.test(id)) {
+        const records = parse(src, { columns })
+
+        return {
+          code: `export default ${JSON.stringify(records)}`
+        }
       }
     }
+    // transformIndexHtml(html) {
+    //   console.log('--------------------------------------');
+    //   return html.replace(/<\/body>/, `<script>alert('aa')</script><\/body>`)
+    // },
   }
-})
+}
