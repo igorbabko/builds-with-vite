097ca968186fb4f3fe5f792392e2aaaf6d27a5e9
Igor Babko <i.i.babko@gmail.com>
2023-09-26 10:04:31 +0300

    30. Dispose HMR updates

--- 27-hmr/module.js
+++ 27-hmr/module.js
@@ -6,6 +6,22 @@ export default 'default message from module.js'
 
 console.log('from module.js')
 
+let styles
+
+function addStylesheet() {
+  styles = document.createElement('style')
+  
+  styles.innerHTML = 'body { background: indigo; color: yellow; }'
+  
+  document.head.appendChild(styles)
+}
+
+function removeStylesheet() {
+  styles.remove()
+}
+
+addStylesheet()
+
 if (import.meta.hot) {
   import.meta.hot.accept((updatedModule) => {
     if (updatedModule) {
@@ -17,5 +33,21 @@ if (import.meta.hot) {
     if (newSubmodule) {
       console.log(newSubmodule)
     }
+  })
+  
+  import.meta.hot.dispose(() => {
+    removeStylesheet()
+  })
+  
+  import.meta.hot.on('vite:beforeUpdate', (data) => {
+    console.log('before update', data)
+  })
+
+  import.meta.hot.on('vite:afterUpdate', (data) => {
+    console.log('after update', data)
+  })
+
+  import.meta.hot.on('vite:error', (data) => {
+    console.log('error', data)
   })
 }
