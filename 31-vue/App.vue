ec53db76765c7c7382b64660d330796275293b2c
Igor Babko <i.i.babko@gmail.com>
2023-09-28 10:44:16 +0300

    31. Vue integration

---
+++ 31-vue/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>Counter</h1>
+  <button @click="count++">Count: {{ count }}</button>
+</template>
---
+++ 31-vue/vite.config.js
@@ -0,0 +1,6 @@
+import Inspect from 'vite-plugin-inspect'
+import vue from '@vitejs/plugin-vue'
+
+export default {
+  plugins: [Inspect(), vue()]
+}
